876a18007d28891497e890f76dbb8d226e36354b
RIYAN <137081303+rynnn10@users.noreply.github.com>
2025-12-15 23:49:18 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,36 @@
-// GANTI VERSION INI SETIAP KALI UPDATE KODE (Misal: v3, v4, dst)
-const CACHE_NAME = 'sandikata-v3-autoicon'; 
+// GANTI VERSION INI SETIAP KALI UPDATE (Misal: v4, v5, dst)
+const CACHE_NAME = 'sandikata-v4-local-icon'; 
 
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
+  './ico192.png',  // DAFTARKAN ICON 192 DI SINI
+  './ico512.png',  // DAFTARKAN ICON 512 DI SINI
   'https://cdn.tailwindcss.com',
   'https://fonts.googleapis.com/css2?family=Patrick+Hand&display=swap',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-  'https://cdn.pixabay.com/download/audio/2022/05/27/audio_1808fbf07a.mp3',
-  // Kita cache link icon otomatisnya juga:
-  'https://ui-avatars.com/api/?name=SK&background=86efac&color=d946ef&size=192&rounded=true&bold=true&length=2',
-  'https://ui-avatars.com/api/?name=SK&background=86efac&color=d946ef&size=512&rounded=true&bold=true&length=2'
+  'https://cdn.pixabay.com/download/audio/2022/05/27/audio_1808fbf07a.mp3'
 ];
 
-// 1. Install Service Worker
+// 1. Install
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('[Service Worker] Caching assets & icons');
+      console.log('[Service Worker] Caching assets & new icons');
       return cache.addAll(ASSETS_TO_CACHE);
     })
   );
   self.skipWaiting();
 });
 
-// 2. Activate & Bersihkan Cache Lama
+// 2. Activate & Hapus Cache Lama
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(keyList.map((key) => {
         if (key !== CACHE_NAME) {
-          console.log('[Service Worker] Hapus cache lama:', key);
+          console.log('[Service Worker] Clear old cache:', key);
           return caches.delete(key);
         }
       }));
@@ -40,7 +39,7 @@ self.addEventListener('activate', (event) => {
   return self.clients.claim();
 });
 
-// 3. Fetch (Cache First)
+// 3. Fetch
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
